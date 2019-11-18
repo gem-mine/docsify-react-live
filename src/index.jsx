328905f2689acb25d5f51ea6b6f1a0f0e20851a9
yaoj <624975@nd.com>
2019-11-18 09:10:53 +0800

    合并react-live

--- src/index.jsx
+++ src/index.jsx
@@ -1,40 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createComponent } from './component'
+import codeBlockWrapper from './hoc/codeBlockWrapper'
 import './assets/dev.less'
 
 const code = `
-  <className>demo-class</className>
-  <title>测试组件标题</title>
-  <desc>
-  测试组件描述
-  - a
-  - b
-  - c
-  </desc>
-  <style>
-    body {
-      height: 30px;
-    }
-  </style>
-  <script>
-  const { Button } = soui
-
-  export default class App extends React.Component {
-    render () {
-      return <Button type="primary" onClick={() => alert('好的')}>确认</Button>
-    }
+<className>demo-class</className>
+<title>测试组件标题</title>
+<desc>
+测试组件描述
+- a
+- b
+- c
+</desc>
+<style>
+  body {
+    height: 30px;
   }
-  </script>
+</style>
+<script>
+const { Button } = soui
+
+export default class App extends React.Component {
+  render () {
+    return <Button type="primary" onClick={() => alert('好的')}>确认</Button>
+  }
+}
+</script>
 `
 
-const Component = createComponent(code, true)
+const CodeBlockWrapper = codeBlockWrapper(code, 'true')
 
 ReactDOM.render(
-  <Component/>,
+  <CodeBlockWrapper/>,
   document.getElementById('apphook')
 )
 
 module.hot && module.hot.accept()
-
-window.testDemo = 'a'
