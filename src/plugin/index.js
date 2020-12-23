f4a94273f5c65a140e7da7c29a3bfbe0f3bacfe1
githoniel <githoniel88@gmail.com>
2020-12-23 16:15:23 +0800

    fix: 修正错误覆盖原有markdown选项的BUG

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -1,37 +1,36 @@
 import ReactDOM from 'react-dom'
 import codeBlockWrapper from '../hoc/codeBlockWrapper'
 
-
 // eslint-disable-next-line import/prefer-default-export
-export const create = function(scope, theme) {
-  return function() {
+export const createPlugin = function createPlugin(scope, theme) {
+  return function DocisfyReactLive() {
     const idPrefix = 'demo-box-react-'
     let id = 0
-    function renderComponent(Component, id) {
 
-      setTimeout(function () {
-        ReactDOM.render(Component(), document.getElementById(id + ''))
+    function renderComponent(Component, elId) {
+      setTimeout(() => {
+        ReactDOM.render(Component(), document.getElementById(`${elId}`))
       })
     }
 
-    window.$docsify.markdown = {
-      renderer: {
-        code: function(code, lang) {
-          if ((lang === 'jsx' || lang === 'tsx')
+    window.$docsify.markdown = window.$docsify.markdown || {}
+    const markdownConfig = window.$docsify.markdown
+    markdownConfig.render = markdownConfig.render || {}
+
+    markdownConfig.render.code = function renderCode(code, lang, ...rest) {
+      if ((lang === 'jsx' || lang === 'tsx')
             && /^\/\*\s*react(.+)*\s*\*\//.test(code)) {
-            id++
-            const params = code.match(/^\/\*\s*react(.+)*\s*\*\//)[1] || ''
-            const live = params.split(' ').indexOf('live') > -1
-            const pureRender = params.split(' ').indexOf('pureRender') > -1
-            const codeShow = params.split(' ').indexOf('codeShow') > -1
-            const Component = codeBlockWrapper(code, scope, theme, live, pureRender, lang, codeShow)
-            const currentId = `${idPrefix}${id}`
-            renderComponent(Component, currentId)
-            return '<div id="' + currentId + '" class="demo-box demo-box-react"/></div/>'
-          } else {
-            return this.origin.code.apply(this, arguments)
-          }
-        }
+        id += 1
+        const params = code.match(/^\/\*\s*react(.+)*\s*\*\//)[1] || ''
+        const live = params.split(' ').indexOf('live') > -1
+        const pureRender = params.split(' ').indexOf('pureRender') > -1
+        const codeShow = params.split(' ').indexOf('codeShow') > -1
+        const Component = codeBlockWrapper(code, scope, theme, live, pureRender, lang, codeShow)
+        const currentId = `${idPrefix}${id}`
+        renderComponent(Component, currentId)
+        return `<div id="${currentId}" class="demo-box demo-box-react"/></div/>`
+      } else {
+        return this.origin.code(code, lang, ...rest)
       }
     }
   }
