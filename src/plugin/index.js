0aaf00569ceea38c8008603bcc185c5dde91d2c0
githoniel <githoniel88@gmail.com>
2021-01-20 21:32:50 +0800

    feat: 重构插件输出

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -1,45 +1,44 @@
 import ReactDOM from 'react-dom'
 import codeBlockWrapper from '../hoc/codeBlockWrapper'
 
-// eslint-disable-next-line import/prefer-default-export
-export const create = function createPlugin(scope, theme) {
-  return function DocisfyReactLive(hook) {
-    let reactRenderedIds = []
-    const idPrefix = 'demo-box-react-'
-    let id = 0
+let reactRenderedIds = []
+const idPrefix = 'demo-box-react-'
+let id = 0
 
-    function renderComponent(Component, elId) {
-      reactRenderedIds.push(elId)
-      setTimeout(() => {
-        ReactDOM.render(Component(), document.getElementById(elId))
-      })
-    }
+function renderComponent(Component, elId) {
+  reactRenderedIds.push(elId)
+  setTimeout(() => {
+    ReactDOM.render(Component(), document.getElementById(elId))
+  })
+}
 
-    window.$docsify.markdown = window.$docsify.markdown || {}
-    const markdownConfig = window.$docsify.markdown
-    markdownConfig.renderer = markdownConfig.renderer || {}
-    markdownConfig.renderer.code = function renderCode(code, lang, ...rest) {
-      if ((lang === 'jsx' || lang === 'tsx')
-            && /^\/\*\s*react(.+)*\s*\*\//.test(code)) {
-        id += 1
-        const params = code.match(/^\/\*\s*react(.+)*\s*\*\//)[1] || ''
-        const live = params.split(' ').indexOf('live') > -1
-        const pureRender = params.split(' ').indexOf('pureRender') > -1
-        const codeShow = params.split(' ').indexOf('codeShow') > -1
-        const Component = codeBlockWrapper(code, scope, theme, live, pureRender, lang, codeShow)
-        const currentId = `${idPrefix}${id}`
-        renderComponent(Component, currentId)
-        return `<div id="${currentId}" class="demo-box demo-box-react"/></div/>`
-      } else {
-        return this.origin.code(code, lang, ...rest)
-      }
-    }
-
-    hook.beforeEach(() => {
-      reactRenderedIds.forEach((mountedId) => {
-        ReactDOM.unmountComponentAtNode(document.getElementById(mountedId))
-      })
-      reactRenderedIds = []
-    })
+window.$docsify.markdown = window.$docsify.markdown || {}
+const markdownConfig = window.$docsify.markdown
+markdownConfig.renderer = markdownConfig.renderer || {}
+markdownConfig.renderer.code = function renderCode(code, lang, ...rest) {
+  if ((lang === 'jsx' || lang === 'tsx')
+        && /^\/\*\s*react(.+)*\s*\*\//.test(code)) {
+    id += 1
+    const params = code.match(/^\/\*\s*react(.+)*\s*\*\//)[1] || ''
+    const live = params.split(' ').indexOf('live') > -1
+    const pureRender = params.split(' ').indexOf('pureRender') > -1
+    const codeShow = params.split(' ').indexOf('codeShow') > -1
+    const Component = codeBlockWrapper(code, undefined, undefined, live, pureRender, lang, codeShow)
+    const currentId = `${idPrefix}${id}`
+    renderComponent(Component, currentId)
+    return `<div id="${currentId}" class="demo-box demo-box-react"/></div/>`
+  } else {
+    return this.origin.code(code, lang, ...rest)
   }
 }
+
+function install(hook) {
+  hook.beforeEach(() => {
+    reactRenderedIds.forEach((mountedId) => {
+      ReactDOM.unmountComponentAtNode(document.getElementById(mountedId))
+    })
+    reactRenderedIds = []
+  })
+}
+
+window.$docsify.plugins = [].concat(install, window.$docsify.plugins)
